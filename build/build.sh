3da2327c840874706c0f986bc262724b6cbedc2b
Aerostation <thornton@aerostation.org>
2020-06-04 15:41:24 -0400

    Refactor for python package (#3)
    * Split DBI3cli into shim calling dbi3_access/dbi3_main.py.  Package is dbi3_access, can be pip installed, and will create its own DBI3cli main.  build.sh builds both pyinstaller and setuptools output.

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Getting automatic version strings working correctly with both frozen (via pyinstaller) code and development builds
-# has been difficult.  The build script drives pyinstaller using a clean temp directory for the applicaiont file and
-# writes the frozen __version__ file with a __pyinstaller__ flag file so the application doesn't try to runtime
-# update the version via git.
+# has been difficult.  The build script drives pyinstaller using a clean temp directory for the application files and
+# writes the frozen __version__ file.
 
 # This build.sh should be in the repo/build directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -17,18 +16,8 @@ else
     PYINSTALLER_CMD='pyinstaller'
 fi
 
-# Change to the build dir so relative paths will work.
-cd $DIR
-# Remove any existing temporary application tree and ensure we have the top level dir
-rm -rf ./app
-mkdir ./app
-
-# Now copy application files to the clean app sub-directory for pyinstaller to work on
-cp ../DBI3cli app/
-(cd ..; find . -name '*.py' -not -path './build/*' | xargs cp -p --parent -t build/app)
-
-# Write the pyinstaller __version__ file to the app tree
-( cd ..;  # must run in the repo to execute get_version
+# Update the __version__ file to the latest git tag
+( cd "$DIR/.." || return 1  # must run in the repo to execute get_version
 $PYTHON_CMD - <<PyScript
 from __future__ import print_function
 import os
@@ -41,18 +30,30 @@ try:
 except UserWarning:
     print('GIT must be in the path for get_version() to work')
 
-with open(os.path.join('build', 'app', 'lib', '__version__.py'), 'w') as v_file:
+with open(os.path.join('dbi3_access', 'lib', '__version__.py'), 'w') as v_file:
     print('# dev run generated version from git', file=v_file)
     print('__version__ = "{}"'.format(version), file=v_file)
-with open(os.path.join('build', 'app', '__pyinstaller__.py'), 'w') as v_file:
-    print('__pyinstaller__ = "{}"'.format(py_time), file=v_file)
-print('__version__ = {}  __pyinstaller__ = {}'.format(version, py_time))
+print('__version__ = {} '.format(version))
 PyScript
-)
+) || { echo "Create version file failed"; exit 1; }
+
+# Change to the build dir so relative paths will work.
+cd "$DIR" || exit
+# Remove any existing temporary application tree and ensure we have the top level dir
+rm -rf ./app
+mkdir ./app
+
+# Now copy application files to the clean app sub-directory for pyinstaller to work on
+(
+    cd "$DIR/.."
+    cp DBI3cli build/app/
+    find . -name '*.py' -not -path './build/*' -print0 | xargs -0 cp -p --parent -t build/app
+) || { echo "Copy failed"; exit 1; }
 
 # Now execute the appropriate pyinstaller command line for the current OS
+echo "****Run pyinstaller"
 (
-    cd $DIR/app
+    cd "$DIR/app"
     if [[ $OS = 'Windows_NT' ]]
     then
         $PYINSTALLER_CMD --clean --workpath ./work --distpath ./dist --onefile --console DBI3cli
@@ -60,4 +61,10 @@ PyScript
     else
         $PYINSTALLER_CMD --clean --workpath ./work --distpath ./dist --onefile DBI3cli
     fi
-)
+) || echo "Pyinstaller failed"
+# Now create a python application package
+echo "****Run setuptools sdist"
+(
+    cd "$DIR/.."
+    $PYTHON_CMD setup.py sdist
+) || echo "setuptools sdist failed"
